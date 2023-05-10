5a8092f7f5ce54cd830fa519b9defdf099977627
adebayodeji <adebyodeji14@yahoo.com>
2023-05-10 12:21:07 +0200

    arithemetic_operators

---
+++ arithemetic_operators.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_val = (a + b)
+    
+    diff_val = (a - b)
+    
+    mul_val = (a * b)
+    
+    print(sum_val)
+    print(diff_val)
+    print(mul_val)
+O
